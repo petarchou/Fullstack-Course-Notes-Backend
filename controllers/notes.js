1e7b254a9a2ca1e68659f72b1ec201d4e026f497
Magellan <ppetrov12@abv.bg>
2023-12-10 16:01:55 +0200

    add relations between notes and users

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,23 +1,24 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(notes)
 })
 
-notesRouter.get('/:id', async (request, response, next) => {
+notesRouter.get('/:id', async (request, response) => {
   const id = request.params.id;
-  
-    const note = await Note.findById(id)
 
-    if (note) {
-      response.json(note)
-    }
-    else {
-      response.status(404).end();
-    }
-  
+  const note = await Note.findById(id)
+
+  if (note) {
+    response.json(note)
+  }
+  else {
+    response.status(404).end();
+  }
+
 })
 
 notesRouter.delete('/:id', async (request, response) => {
@@ -29,22 +30,28 @@ notesRouter.delete('/:id', async (request, response) => {
 
 })
 
-notesRouter.post('/', async (request, response, next) => {
-  const { content, important } = request.body;
+notesRouter.post('/', async (request, response) => {
+  const { content, important, userId } = request.body;
+
+  const user = await User.findById(userId)
   const note = new Note({
     content,
     important: important || false,
+    user: user.id
   })
-    const savedNote = await note.save()
-    response.status(201).json(savedNote)
+
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-    await Note.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
-notesRouter.put('/:id', async (request, response, next) => {
+notesRouter.put('/:id', async (request, response) => {
   const { content, important } = request.body;
   const note = {
     content,
@@ -55,11 +62,11 @@ notesRouter.put('/:id', async (request, response, next) => {
     runValidators: true,
     context: 'query'
   })
-      if (!updatedNote) {
-        //throw error for not existing note
-        return response.status(404).json({error:'Note not found'})
-      }
-      response.json(updatedNote)
+  if (!updatedNote) {
+    //throw error for not existing note
+    return response.status(404).json({ error: 'Note not found' })
+  }
+  response.json(updatedNote)
 })
 
 module.exports = notesRouter;
